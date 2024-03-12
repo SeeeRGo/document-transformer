7b4062d3708520cb5aca2a3735babd86723f02cc
Мизин Сергей Юрьевич (4091265) <mizin@vtb.ru>
2024-03-12 13:30:52 +0400

    rag demo

--- src/app/api/get-answers/route.ts
+++ src/app/api/get-answers/route.ts
@@ -1,20 +1,91 @@
 import { NextRequest } from 'next/server';
 import OpenAI from 'openai';
+import { ChatOpenAI, OpenAIEmbeddings } from '@langchain/openai';
+import { ChatPromptTemplate } from '@langchain/core/prompts';
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents"
+import { createRetrievalChain } from "langchain/chains/retrieval"
+import { DocxLoader } from "langchain/document_loaders/fs/docx";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
+import { MemoryVectorStore } from "langchain/vectorstores/memory"
 
 // gets API Key from environment variable OPENAI_API_KEY
 const client = new OpenAI();
 export async function POST(request: NextRequest) {
-  const text = await request.json()
+  const { text } = await request.json()
+  console.log('text', text);
+  
+  const model = new ChatOpenAI({
+    modelName: "gpt-3.5-turbo",
+    temperature: 0.7,
+  })
+  
+  // Add metadata
+  const prompt = ChatPromptTemplate.fromTemplate(`
+    Answer the user's question. 
+    Context: {context}
+    Question: {input}
+  `)
 
-  const questions = await client.chat.completions.create({
-    model: 'gpt-3.5-turbo-0125',
-    messages: [
-      {"role": "system", "content": "You are a helpful assistant finding questions to be answered to fill documentsю separate questions with neww lines"},
-      {"role": "user", "content": `На какие вопросы надо найти ответы чтобы заполнить этот документ? 
-      ${text}`,}
-    ],
+  const loader1 = new DocxLoader(
+    "src/documents/1.docx"
+  );
+  
+  const loader2 = new DocxLoader(
+    "src/documents/2.docx"
+  );
+  
+  const loader3 = new DocxLoader(
+    "src/documents/3.docx"
+  );
+  
+  const loader4 = new DocxLoader(
+    "src/documents/4.docx"
+  );
+  
+  const doc1 = await loader1.load();
+  const doc2 = await loader2.load();
+  const doc3 = await loader3.load();
+  const doc4 = await loader4.load();
+    
+  const chain = await createStuffDocumentsChain({
+    llm: model,
+    prompt,
   })
 
-  return Response.json({ data: questions.choices.at(0)?.message.content ?? '' })
+  const context = [...doc1, ...doc2, ...doc3, ...doc4]
+
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 40,
+  })
+
+  const splitDocs = await splitter.splitDocuments(context)
+
+  const embeddings = new OpenAIEmbeddings();
+
+  const vectorStore = await MemoryVectorStore.fromDocuments(splitDocs, embeddings)
+
+  // Retrieve data
+  const retriever = vectorStore.asRetriever();
+
+  const retrievalChain = await createRetrievalChain({
+    retriever,
+     combineDocsChain: chain
+  })
+
+  const response = await retrievalChain.invoke({
+    input: text,
+  })  
+
+  // const questions = await client.chat.completions.create({
+  //   model: 'gpt-3.5-turbo-0125',
+  //   messages: [
+  //     {"role": "system", "content": "You are a helpful assistant finding questions to be answered to fill documentsю separate questions with neww lines"},
+  //     {"role": "user", "content": `На какие вопросы надо найти ответы чтобы заполнить этот документ? 
+  //     ${text}`,}
+  //   ],
+  // })
+
+  return Response.json({ data: response.answer })
 
 }
--- src/app/compile-answers/page.tsx
+++ src/app/compile-answers/page.tsx
@@ -1,68 +1,39 @@
 "use client"
 import { CloseOutlined, FileUploadOutlined } from "@mui/icons-material";
-import { Button, FormControl, FormHelperText, Stack, Typography } from "@mui/material";
+import { Button, FormControl, FormHelperText, Stack, TextField, Typography } from "@mui/material";
 import axios from "axios";
-import { Controller, useForm } from "react-hook-form";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 interface Inputs {
-  fileToComplete: File | undefined
+  question: string
 }
 
 export default function CompileAnswers() {
   const {
     handleSubmit,
-    control,
-    watch,
-    setValue,
+    register,
     formState: { errors },
   } = useForm<Inputs>()
-  const fileName = watch('fileToComplete')
+
+  const [answer, setAnswer] = useState('')
 
   return (
     <Stack rowGap={1}>
-      <Controller 
-        control={control}
-        name="fileToComplete"
-        rules={{ required: 'Поле обязательно к заполнению' }}
-        render={
-        ({ field: { value, onChange, ...field } }) => {          
-          return (
-            <FormControl error={!!errors.fileToComplete?.message}>
-                <input
-                  style={{ display: "none" }}
-                  type="file"
-                  hidden
-                  id="compile-input"
-                  accept=".docx,application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-                  onChange={(event) => {
-                    if (event.target.files) {
-                      onChange(event.target.files[0]);
-                    }}}
-                  {...field} 
-                />
-                <Button startIcon={<FileUploadOutlined />} onClick={() => document.getElementById("compile-input")?.click()} variant="outlined">{"Файл в формате docx"}</Button>
-                {errors.fileToComplete?.message && <FormHelperText>{errors.fileToComplete?.message}</FormHelperText>}
-              </FormControl>
-          );
-        }
-      } />
-      {fileName && <Stack direction="row" justifyContent="space-between"><Typography>{fileName.name}</Typography><CloseOutlined onClick={() => { setValue('fileToComplete', undefined) }}/></Stack>}
+      <TextField {...register("question")} />
       <Button
           variant="contained"
           onClick={() => {
-            handleSubmit(async ({ fileToComplete }) => {
-              const formData = new FormData()
-              if (fileToComplete) {
-                formData.append('resume', fileToComplete)
-              }
-              const { data: { message: text }}: {data: { message: string }} = await axios.post('/api/extract', formData)
-              const { data: { data: message} }: { data: { data: string } } = await axios.post('/api/get-answers', { text })
-              console.log('questions', message.split('\n'));              
+            handleSubmit(async ({ question }) => {
+              setAnswer('')
+              const { data: { data } }: { data: { data: string } } = await axios.post('/api/get-answers', { text: question })
+              setAnswer(data)             
             })()
           }}        
         >
-          Найти вопросы
+          Найти ответ
         </Button>
+        {answer ? <Typography>{answer}</Typography> : null}
     </Stack>
   )
 }
