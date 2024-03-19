f072fe72a0d67b20db8f4a90b8cc9ad64cadc725
Мизин Сергей Юрьевич (4091265) <mizin@vtb.ru>
2024-03-19 19:35:38 +0400

    backend compatible get answers func

--- src/app/api/get-answers/route.ts
+++ src/app/api/get-answers/route.ts
@@ -4,9 +4,8 @@ import { ChatOpenAI, OpenAIEmbeddings } from '@langchain/openai';
 import { ChatPromptTemplate } from '@langchain/core/prompts';
 import { createStuffDocumentsChain } from "langchain/chains/combine_documents"
 import { createRetrievalChain } from "langchain/chains/retrieval"
-import { DocxLoader } from "langchain/document_loaders/fs/docx";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
-import { MemoryVectorStore } from "langchain/vectorstores/memory"
+import { SupabaseVectorStore } from '@langchain/community/vectorstores/supabase';
+import { supabase } from '@/utils/db';
 
 // gets API Key from environment variable OPENAI_API_KEY
 const client = new OpenAI();
@@ -24,45 +23,18 @@ export async function POST(request: NextRequest) {
     Context: {context}
     Question: {input}
   `)
-
-  const loader1 = new DocxLoader(
-    "src/documents/1.docx"
-  );
-  
-  const loader2 = new DocxLoader(
-    "src/documents/2.docx"
-  );
-  
-  const loader3 = new DocxLoader(
-    "src/documents/3.docx"
-  );
-  
-  const loader4 = new DocxLoader(
-    "src/documents/4.docx"
-  );
-  
-  const doc1 = await loader1.load();
-  const doc2 = await loader2.load();
-  const doc3 = await loader3.load();
-  const doc4 = await loader4.load();
     
   const chain = await createStuffDocumentsChain({
     llm: model,
     prompt,
   })
 
-  const context = [...doc1, ...doc2, ...doc3, ...doc4]
-
-  const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 1000,
-    chunkOverlap: 200,
-  })
-
-  const splitDocs = await splitter.splitDocuments(context)
-
   const embeddings = new OpenAIEmbeddings();
 
-  const vectorStore = await MemoryVectorStore.fromDocuments(splitDocs, embeddings)
+  const vectorStore = new SupabaseVectorStore(embeddings, {
+    client: supabase,
+    tableName: "documents",
+  });
 
   // Retrieve data
   const retriever = vectorStore.asRetriever();
@@ -75,15 +47,6 @@ export async function POST(request: NextRequest) {
   const response = await retrievalChain.invoke({
     input: text,
   })  
-
-  // const questions = await client.chat.completions.create({
-  //   model: 'gpt-3.5-turbo-0125',
-  //   messages: [
-  //     {"role": "system", "content": "You are a helpful assistant finding questions to be answered to fill documentsю separate questions with neww lines"},
-  //     {"role": "user", "content": `На какие вопросы надо найти ответы чтобы заполнить этот документ? 
-  //     ${text}`,}
-  //   ],
-  // })
 
   return Response.json({ data: response.answer })
 
