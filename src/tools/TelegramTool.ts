d42b6c5057a6f776549ccc395de6d20bd02d971f
Мизин Сергей Юрьевич (4091265) <mizin@vtb.ru>
2024-03-22 18:05:22 +0400

    note to self

--- src/tools/TelegramTool.ts
+++ src/tools/TelegramTool.ts
@@ -2,6 +2,7 @@ import { Tool } from "@langchain/core/tools";
 import { CallbackManagerForToolRun } from '@langchain/core/callbacks/manager'
 import { RunnableConfig } from '@langchain/core/runnables'
 
+// Dynamic tools are enough
 export class TelegramMessagesTool extends Tool {
   protected _call(arg: any, runManager?: CallbackManagerForToolRun | undefined, config?: RunnableConfig | undefined): Promise<string> {    
     return Promise.resolve('')
