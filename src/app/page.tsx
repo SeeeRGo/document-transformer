107746d5cafabd38bfc6c74529aee6a90a98e686
Sergey Mizin <seeergo@yahoo.com>
2025-02-24 10:20:51 +0300

    plugged back in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -131,158 +131,158 @@ const createFunction = ({
 const getExtension = (type?: string) => type === 'application/pdf' ? 'pdf' : 'docx'
 // Create a single supabase client for interacting with your database
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL ?? '', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '')
-// export default function CosysoftTemplate() {
-//   const {
-//     handleSubmit,
-//     control,
-//     watch,
-//     setValue,
-//     formState: { errors },
-//   } = useForm<Inputs>()
-//   const [isLoading, setIsLoading] = useState(false)
-//   const [error, setError] = useState('')  
-//   const fileName = watch('resume')
-//   // const [data, setData] = useState(INITIAL_DATA);
-//   const isDev = process.env.NODE_ENV === 'development'  
+export default function CosysoftTemplate() {
+  const {
+    handleSubmit,
+    control,
+    watch,
+    setValue,
+    formState: { errors },
+  } = useForm<Inputs>()
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState('')  
+  const fileName = watch('resume')
+  // const [data, setData] = useState(INITIAL_DATA);
+  const isDev = process.env.NODE_ENV === 'development'  
 
-//   return (
-//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-//       {isLoading ? (
-//       <div style={{ display: 'flex', width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center'}}>
-//         <CircularProgress />
-//       </div>
-//     ) : (
-//       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-//         <StyledStack rowGap={2}>
-//           <Controller 
-//             control={control}
-//             name="resume"
-//             rules={{ required: 'Поле обязательно к заполнению' }}
-//             render={
-//             ({ field: { value, onChange, ...field } }) => {          
-//               return (
-//                 <FormControl error={!!errors.resume?.message}>
-//                     <input
-//                       style={{ display: "none" }}
-//                       type="file"
-//                       hidden
-//                       id="file-input"
-//                       accept=".docx,application/pdf,application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-//                       onChange={(event) => {
-//                         if (event.target.files) {
-//                           onChange(event.target.files[0]);
-//                         }}}
-//                       {...field} 
-//                     />
-//                     <Button startIcon={<FileUploadOutlined />} onClick={() => document.getElementById("file-input")?.click()} variant="outlined">{"Файл резюме в формате docx, pdf"}</Button>
-//                     {errors.resume?.message && <FormHelperText>{errors.resume?.message}</FormHelperText>}
-//                   </FormControl>
-//               );
-//             }
-//           } />
-//           {fileName && <Stack direction="row" justifyContent="space-between"><Typography>{fileName.name}</Typography><CloseOutlined onClick={() => { setValue('resume', undefined) }}/></Stack>}
-//           <Controller
-//             name="templateType"
-//             control={control}
-//             render={({ field: { value, ref, ...field } }) => {
-//               return (
-//                 <FormControl>
-//                   <InputLabel>Формат</InputLabel>
-//                   <Select
-//                     {...field}
-//                     inputRef={ref}
-//                     label="Формат"
-//                     defaultValue={[]}
-//                     value={value}
-//                     color="primary"
-//                     size={"medium"}
-//                     multiple
-//                   >
-//                     {Object.entries(TEMPLATE_TYPE_OPTIONS).map(([key, value]) => (
-//                       <MenuItem key={key} value={key}>{value}</MenuItem>
-//                     ))}
-//                   </Select>
-//               </FormControl>
-//               );
-//             }}
-//           />
-//         <Button
-//           variant="contained"
-//           onClick={() => {
-//             setIsLoading(true)
-//             handleSubmit(async ({ resume, templateType }) => {
-//               const formData = new FormData()
-//               const currentTime = dayjs().toISOString() // TODO make folders by day
-//               const currentDay = dayjs().format('DD-MM-YYYY')
-//               const originalName = `${isDev ? 'dev/': ''}${currentDay}/${currentTime}-original.${getExtension(resume?.type)}`
-//               const processedName = `${isDev ? 'dev/': ''}${currentDay}/${currentTime}-processed.docx`
-//               let originalLink = ''
-//               if (resume) {
-//                 formData.append('resume', resume)
-                
-//                 await supabase.storage.from(bucketName).upload(originalName, resume)
-//                 const { data: { publicUrl } } = supabase
-//                   .storage
-//                   .from(bucketName)
-//                   .getPublicUrl(originalName, {
-//                     download: true,
-//                   })
-//                 originalLink = publicUrl
-//               }
-//               try {
-//                 const { data: { message: text }}: {data: { message: string }} = await axios.post('/api/extract', formData)
-//                 Promise.allSettled(templateType.map(type => createFunction({ originalLink, processedName, text, templateType: type })
-//                 .catch(e => {
-//                   console.log('error', e);
-//                   setError('Произошла ошибка в процессе конвертации резюме, пожалуйста, попробуйте ещё раз')
-//                 }))).then(() => { setIsLoading(false) })
-//               } catch (e) {
-//                 console.log('error', e);
-//                 setError('Произошла ошибка в процессе парсинга резюме, пожалуйста, попробуйте ещё раз')
-//                 setIsLoading(false)
-//               }
-              
-//               // Promise.allSettled(templateType.map(type => {
-//               //   return axios.post('/api', formData)
-//               //   .then(async ({data: { message, length }}) => {
-//               //     if (type === 'NLMK') {
-//               //       const file = await createNlmkFile(message ?? '')
-//               //       return {
-//               //         ...file,
-//               //         length,
-//               //       }
-//               //     } else {
-//               //       const file = await createFile(message ?? '', type === 'CosysoftBranded')
-//               //       return {
-//               //         ...file,
-//               //         length,
-//               //       }
-//               //     }
-//               //   })
-//               //   .then(async ({ blob, name, length }) => {
-//               //     saveAs(blob, `${name}.docx`)
-//               //   })
-//               // }))
-//             })()
-//           }}        
-//         >
-//           Конвертировать CV в другой формат
-//         </Button>
-//         </StyledStack>
-//       </div>
-//     )
-//   }
-//       <Snackbar open={!!error} autoHideDuration={6000} onClose={() => { setError('') }}>
-//         <Alert onClose={() => { setError('') }} severity="error" sx={{ width: '100%' }}>
-//           {error}
-//         </Alert>
-//       </Snackbar>
-//     </main>
-//   );
-// }
-
-export default function HomePage() {
   return (
-    <h2>Проект в процессе переезда если вы видите это сообщение, значит я слишком ленивый чтобы сделать это незаметно, пните мення в телеге или ИРЛ</h2>
-  )
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {isLoading ? (
+      <div style={{ display: 'flex', width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center'}}>
+        <CircularProgress />
+      </div>
+    ) : (
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <StyledStack rowGap={2}>
+          <Controller 
+            control={control}
+            name="resume"
+            rules={{ required: 'Поле обязательно к заполнению' }}
+            render={
+            ({ field: { value, onChange, ...field } }) => {          
+              return (
+                <FormControl error={!!errors.resume?.message}>
+                    <input
+                      style={{ display: "none" }}
+                      type="file"
+                      hidden
+                      id="file-input"
+                      accept=".docx,application/pdf,application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+                      onChange={(event) => {
+                        if (event.target.files) {
+                          onChange(event.target.files[0]);
+                        }}}
+                      {...field} 
+                    />
+                    <Button startIcon={<FileUploadOutlined />} onClick={() => document.getElementById("file-input")?.click()} variant="outlined">{"Файл резюме в формате docx, pdf"}</Button>
+                    {errors.resume?.message && <FormHelperText>{errors.resume?.message}</FormHelperText>}
+                  </FormControl>
+              );
+            }
+          } />
+          {fileName && <Stack direction="row" justifyContent="space-between"><Typography>{fileName.name}</Typography><CloseOutlined onClick={() => { setValue('resume', undefined) }}/></Stack>}
+          <Controller
+            name="templateType"
+            control={control}
+            render={({ field: { value, ref, ...field } }) => {
+              return (
+                <FormControl>
+                  <InputLabel>Формат</InputLabel>
+                  <Select
+                    {...field}
+                    inputRef={ref}
+                    label="Формат"
+                    defaultValue={[]}
+                    value={value}
+                    color="primary"
+                    size={"medium"}
+                    multiple
+                  >
+                    {Object.entries(TEMPLATE_TYPE_OPTIONS).map(([key, value]) => (
+                      <MenuItem key={key} value={key}>{value}</MenuItem>
+                    ))}
+                  </Select>
+              </FormControl>
+              );
+            }}
+          />
+        <Button
+          variant="contained"
+          onClick={() => {
+            setIsLoading(true)
+            handleSubmit(async ({ resume, templateType }) => {
+              const formData = new FormData()
+              const currentTime = dayjs().toISOString() // TODO make folders by day
+              const currentDay = dayjs().format('DD-MM-YYYY')
+              const originalName = `${isDev ? 'dev/': ''}${currentDay}/${currentTime}-original.${getExtension(resume?.type)}`
+              const processedName = `${isDev ? 'dev/': ''}${currentDay}/${currentTime}-processed.docx`
+              let originalLink = ''
+              if (resume) {
+                formData.append('resume', resume)
+                
+                await supabase.storage.from(bucketName).upload(originalName, resume)
+                const { data: { publicUrl } } = supabase
+                  .storage
+                  .from(bucketName)
+                  .getPublicUrl(originalName, {
+                    download: true,
+                  })
+                originalLink = publicUrl
+              }
+              try {
+                const { data: { message: text }}: {data: { message: string }} = await axios.post('/api/extract', formData)
+                Promise.allSettled(templateType.map(type => createFunction({ originalLink, processedName, text, templateType: type })
+                .catch(e => {
+                  console.log('error', e);
+                  setError('Произошла ошибка в процессе конвертации резюме, пожалуйста, попробуйте ещё раз')
+                }))).then(() => { setIsLoading(false) })
+              } catch (e) {
+                console.log('error', e);
+                setError('Произошла ошибка в процессе парсинга резюме, пожалуйста, попробуйте ещё раз')
+                setIsLoading(false)
+              }
+              
+              // Promise.allSettled(templateType.map(type => {
+              //   return axios.post('/api', formData)
+              //   .then(async ({data: { message, length }}) => {
+              //     if (type === 'NLMK') {
+              //       const file = await createNlmkFile(message ?? '')
+              //       return {
+              //         ...file,
+              //         length,
+              //       }
+              //     } else {
+              //       const file = await createFile(message ?? '', type === 'CosysoftBranded')
+              //       return {
+              //         ...file,
+              //         length,
+              //       }
+              //     }
+              //   })
+              //   .then(async ({ blob, name, length }) => {
+              //     saveAs(blob, `${name}.docx`)
+              //   })
+              // }))
+            })()
+          }}        
+        >
+          Конвертировать CV в другой формат
+        </Button>
+        </StyledStack>
+      </div>
+    )
+  }
+      <Snackbar open={!!error} autoHideDuration={6000} onClose={() => { setError('') }}>
+        <Alert onClose={() => { setError('') }} severity="error" sx={{ width: '100%' }}>
+          {error}
+        </Alert>
+      </Snackbar>
+    </main>
+  );
 }
+
+// export default function HomePage() {
+//   return (
+//     <h2>Проект в процессе переезда если вы видите это сообщение, значит я слишком ленивый чтобы сделать это незаметно, пните мення в телеге или ИРЛ</h2>
+//   )
+// }
