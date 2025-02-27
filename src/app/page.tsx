77dc257102fa6a9ad034c8b3aeab7736d511acf5
Мизин Сергей Юрьевич (4091265) <seeergo@yahoo.com>
2025-02-27 15:13:46 +0400

    Fixes after moving to other project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import { Controller, useForm } from "react-hook-form"
 import { createFile, createNlmkFile } from "@/utils/createFile";
 import { useState } from "react";
 import { createClient } from '@supabase/supabase-js'
-import dayjs from 'dayjs'
 import { CloseOutlined } from "@mui/icons-material";
 
 type TemplateType = 'Cosysoft' | 'NLMK' | 'CosysoftBranded'
@@ -25,9 +24,6 @@ interface Inputs {
 const parsedJsonMock = {
   name: "Фамилий Имён Отчествович"
 }
-
-const bucketName = 'CV'
-const tableName = 'processed_files'
 
 const simplestTemplate = [
   {
@@ -82,15 +78,10 @@ const StyledStack = styled(Stack)<StackProps>(({ theme }) => ({
 
 const INITIAL_DATA = createEditorJsFromTemplate(parsedJsonMock, simplestTemplate);
 interface FileCreateParams {
-  originalLink: string
-  processedName: string
   text: string
   templateType: TemplateType
 }
-const filenamePrefix = (type: TemplateType) => type === 'NLMK' ? 'nlmk' : type === 'Cosysoft' ? 'cosy' : 'cosy-branded'
 const createFunction = ({
-  originalLink,
-  processedName,
   text,
   templateType
 }: FileCreateParams) => {
@@ -110,25 +101,10 @@ const createFunction = ({
       }
     }
   })
-  .then(async ({ blob, name, length }) => {
+  .then(async ({ blob, name }) => {
     saveAs(blob, `${name}.docx`);
-    await supabase.storage.from(bucketName).upload(`${filenamePrefix(templateType)}-${processedName}`, blob)
-    const { data: { publicUrl: processedLink } } = supabase
-      .storage
-      .from(bucketName)
-      .getPublicUrl(processedName, {
-        download: true,
-      })
-      
-    await supabase.from(tableName).insert({
-      original_url: originalLink,
-      processed_url: processedLink,
-      token_length: length,
-      name,
-    })
   })
 }
-const getExtension = (type?: string) => type === 'application/pdf' ? 'pdf' : 'docx'
 // Create a single supabase client for interacting with your database
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL ?? '', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '')
 export default function CosysoftTemplate() {
@@ -212,26 +188,12 @@ export default function CosysoftTemplate() {
             setIsLoading(true)
             handleSubmit(async ({ resume, templateType }) => {
               const formData = new FormData()
-              const currentTime = dayjs().toISOString() // TODO make folders by day
-              const currentDay = dayjs().format('DD-MM-YYYY')
-              const originalName = `${isDev ? 'dev/': ''}${currentDay}/${currentTime}-original.${getExtension(resume?.type)}`
-              const processedName = `${isDev ? 'dev/': ''}${currentDay}/${currentTime}-processed.docx`
-              let originalLink = ''
               if (resume) {
                 formData.append('resume', resume)
-                
-                await supabase.storage.from(bucketName).upload(originalName, resume)
-                const { data: { publicUrl } } = supabase
-                  .storage
-                  .from(bucketName)
-                  .getPublicUrl(originalName, {
-                    download: true,
-                  })
-                originalLink = publicUrl
               }
               try {
                 const { data: { message: text }}: {data: { message: string }} = await axios.post('/api/extract', formData)
-                Promise.allSettled(templateType.map(type => createFunction({ originalLink, processedName, text, templateType: type })
+                Promise.allSettled(templateType.map(type => createFunction({ text, templateType: type })
                 .catch(e => {
                   console.log('error', e);
                   setError('Произошла ошибка в процессе конвертации резюме, пожалуйста, попробуйте ещё раз')
